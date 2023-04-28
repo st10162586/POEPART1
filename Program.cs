9f3f1b9eb74baa7dd47b146b72920f7b87698a4f
st10162586 <st10162586@imconnect.edu.za>
2023-04-28 10:17:29 +0200

    POEPART 1
    adding a menu class and and run() method

--- Program.cs
+++ Program.cs
@@ -22,219 +22,242 @@ namespace POEPART1
         }
     }
 
-
-
-
-
-    class Creation
+    class Menu1
     {
-
-
-        String recipeN; // recipe name 
-        int nIngredients; // number of ingrediants 
-        int numS; // number of steps 
-        int scale; // to scale 
-
-        int c = 0; // count to start positioning in the first loop 
-        int s = 0; // count for the second loop
-
-        // count keeping users from being confused 
-        int count = 1;
-        int index = 1;
-        
-        //questions answers from user input for scales and clearance 
-        String q1;
-        String q2;
-        String q3;
-
-        //intislising of arrays 
-        private string[] listOfI;
-        private int[] iQuantity;
-        private  string[] ingredientM ;
-        private string[] steps;
-
-
-
-        public void Recipe()
-
+        public void Run()
         {
-            Menu2 m = new Menu2();
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Creation c = new Creation(); // creating a constructor for creation class to use its methods 
 
-            Console.WriteLine("what is the name of the recipe");
-            recipeN = Console.ReadLine();
+            // creating a  menu for the user to enter software
+            Console.WriteLine("what would you like to do (choose a number)");
+            Console.WriteLine("1 : enter recipe");
+            Console.WriteLine("2 : scale recipe");
+            Console.WriteLine("3 : reset to original scale ");
+            Console.WriteLine("4 : clear data");
+            Console.WriteLine("5 : exit");
+            ///sdsfeetgryryhhf
+            int menu;
+            menu = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2 : display");
+            Console.WriteLine("3 : scale recipe");
+            Console.WriteLine("4 : reset to original scale ");
+            Console.WriteLine("5 : clear data");
+            Console.WriteLine("6 : exit");
+        }
+    }
+
+        class Creation
+        {
 
 
-            Console.WriteLine("enter the number of ingredients needed for the recipe");
-            nIngredients = Convert.ToInt32(Console.ReadLine());
+            String recipeN; // recipe name 
+            int nIngredients; // number of ingrediants 
+            int numS; // number of steps 
+            int scale; // to scale 
 
-            // decleration of arrays along with their size using input number of ingredients.
-            listOfI = new string[nIngredients];
-            iQuantity = new int[nIngredients];
-            ingredientM = new string[nIngredients];
+            int c = 0; // count to start positioning in the first loop 
+            int s = 0; // count for the second loop
 
-            while (c < nIngredients)
+            // count keeping users from being confused 
+            int count = 1;
+            int index = 1;
+
+            //questions answers from user input for scales and clearance 
+            String q1;
+            String q2;
+            String q3;
+
+            //intislising of arrays 
+            private string[] listOfI;
+            private int[] iQuantity;
+            private string[] ingredientM;
+            private string[] steps;
+
+
+
+            public void Recipe()
+
             {
-                Console.WriteLine("name of the ingredient" + "" + count); // eg flour
-                listOfI[c] = Console.ReadLine();
+                Menu2 m = new Menu2();
 
-                Console.WriteLine("what is the quantity of the ingrediant" + "" + count + ""); // eg (which would make it 2 flour or 0.2 flour)
-                iQuantity[c] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("what is the name of the recipe");
+                recipeN = Console.ReadLine();
 
-                bool flag = false;
 
-                while (flag)
+                Console.WriteLine("enter the number of ingredients needed for the recipe");
+                nIngredients = Convert.ToInt32(Console.ReadLine());
+
+                // decleration of arrays along with their size using input number of ingredients.
+                listOfI = new string[nIngredients];
+                iQuantity = new int[nIngredients];
+                ingredientM = new string[nIngredients];
+
+                while (c < nIngredients)
                 {
-                    Console.WriteLine("Units of measurment: \n1. cup  \n2. tablespoon \n3. teaspoon ");                    
-                    ingredientM[c] = Console.ReadLine();
+                    Console.WriteLine("name of the ingredient" + "" + count); // eg flour
+                    listOfI[c] = Console.ReadLine();
 
-                    switch (ingredientM[c])
+                    Console.WriteLine("what is the quantity of the ingrediant" + "" + count + ""); // eg (which would make it 2 flour or 0.2 flour)
+                    iQuantity[c] = Convert.ToInt32(Console.ReadLine());
+
+                    bool flag = false;
+
+                    while (flag)
                     {
+                        Console.WriteLine("Units of measurment: \n1. cup  \n2. tablespoon \n3. teaspoon ");
+                        ingredientM[c] = Console.ReadLine();
 
-                        case "1":
-                            ingredientM[c] = "cup";
-                            break;
+                        switch (ingredientM[c])
+                        {
 
-                        case "2":
-                            ingredientM[c] = "tablespoon";
-                            break;
+                            case "1":
+                                ingredientM[c] = "cup";
+                                break;
 
-                        case "3":
-                            ingredientM[c] = "teaspoon";
-                            break;
-                        default:
-                            Console.WriteLine("Please enter the number of the value you want to enter");
-                            break;
+                            case "2":
+                                ingredientM[c] = "tablespoon";
+                                break;
+
+                            case "3":
+                                ingredientM[c] = "teaspoon";
+                                break;
+                            default:
+                                Console.WriteLine("Please enter the number of the value you want to enter");
+                                break;
+
+                        }
 
                     }
 
+
+                    count++;
+                    c++;
+                    // note to self do not add break when creating such a loop
+
                 }
-               
-
-                count++;
-                c++;
-                // note to self do not add break when creating such a loop
-
-            }
 
 
-            Console.WriteLine("how many steps does it take to make the recipe ");
-            numS = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("how many steps does it take to make the recipe ");
+                numS = Convert.ToInt32(Console.ReadLine());
 
-            // adding a size to array according to the amount of steps user input
-            steps = new String[numS];
-         
+                // adding a size to array according to the amount of steps user input
+                steps = new String[numS];
 
-            while (s < numS)
-            {
-                Console.WriteLine("please enter step" + index + " in detail on what should be done in recipe");
-                steps[s] = Console.ReadLine();
 
-                index++;
-                s++;
+                while (s < numS)
+                {
+                    Console.WriteLine("please enter step" + index + " in detail on what should be done in recipe");
+                    steps[s] = Console.ReadLine();
+
+                    index++;
+                    s++;
+
+                }
+
+                Console.WriteLine("");
 
             }
 
-            Console.WriteLine("");
-
-        }
-
-        public void Display()
-        {
-            
-            Console.WriteLine("name of the recipe:" +" "+ recipeN);
-            Console.WriteLine("");
-            Console.WriteLine("list of ingredients:");
-
-            for(int a = 0; a < listOfI.Length; a++) // forloop searching in the array 
+            public void Display()
             {
-                
-               
-                Console.WriteLine(listOfI[a]); // display of Ingredients
 
-                
-            }
-            Console.WriteLine("");
-            Console.WriteLine("the steps needed to be taken for recipe:");
+                Console.WriteLine("name of the recipe:" + " " + recipeN);
+                Console.WriteLine("");
+                Console.WriteLine("list of ingredients:");
 
-            for(int b = 0; b< steps.Length; b++) 
-            {
-                
-                Console.WriteLine(steps[b]);
-
-                
-            }
-
-
-        }
-        public void Scale()
-        { 
-            // recieveing user input if they want to scale
-            Console.WriteLine("");
-            Console.WriteLine("would you like to Scale your recipe yes or no");
-             q1 =Console.ReadLine();
-
-            if (q1.ToLower() == "yes")
-            {
-                Console.WriteLine(" scale quantity of recipe by 0.5(half) , 2 (double) 0r 3(triple)");
-                Console.WriteLine("choose from the 3 above options");
-                Convert.ToDouble(scale);
-                scale = Convert.ToInt32(Console.ReadLine()); 
-              
-
-
-                for (int i = 0; i < iQuantity.Length; i++)
+                for (int a = 0; a < listOfI.Length; a++) // forloop searching in the array 
                 {
 
-                    Console.WriteLine(listOfI[i]);
-                    Console.WriteLine(iQuantity[i] *=  scale);
+
+                    Console.WriteLine(listOfI[a]); // display of Ingredients
+
 
                 }
-            }else if (q1.ToLower() == "no")
-            {
-                Clear();
-            }
+                Console.WriteLine("");
+                Console.WriteLine("the steps needed to be taken for recipe:");
 
-        }
-        public void Reset()
-        {
-            Console.WriteLine("would you like to Scale back to your original  recipe yes or no");
-             q2 = Console.ReadLine();
-
-            if (q2.ToLower() == "yes")
-            {
-                for (int i = 0; i < iQuantity.Length; i++)
+                for (int b = 0; b < steps.Length; b++)
                 {
-                    Console.WriteLine(listOfI[i]);
-                    Console.WriteLine(iQuantity[i] /= scale);
+
+                    Console.WriteLine(steps[b]);
+
+
                 }
-            }else if (q2.ToLower() == "no")
-            {
-                Clear();
+
+
             }
+            public void Scale()
+            {
+                // recieveing user input if they want to scale
+                Console.WriteLine("");
+                Console.WriteLine("would you like to Scale your recipe yes or no");
+                q1 = Console.ReadLine();
+
+                if (q1.ToLower() == "yes")
+                {
+                    Console.WriteLine(" scale quantity of recipe by 0.5(half) , 2 (double) 0r 3(triple)");
+                    Console.WriteLine("choose from the 3 above options");
+                    Convert.ToDouble(scale);
+                    scale = Convert.ToInt32(Console.ReadLine());
+
+
+
+                    for (int i = 0; i < iQuantity.Length; i++)
+                    {
+
+                        Console.WriteLine(listOfI[i]);
+                        Console.WriteLine(iQuantity[i] *= scale);
+
+                    }
+                }
+                else if (q1.ToLower() == "no")
+                {
+                    Clear();
+                }
+
+            }
+            public void Reset()
+            {
+                Console.WriteLine("would you like to Scale back to your original  recipe yes or no");
+                q2 = Console.ReadLine();
+
+                if (q2.ToLower() == "yes")
+                {
+                    for (int i = 0; i < iQuantity.Length; i++)
+                    {
+                        Console.WriteLine(listOfI[i]);
+                        Console.WriteLine(iQuantity[i] /= scale);
+                    }
+                }
+                else if (q2.ToLower() == "no")
+                {
+                    Clear();
+                }
+            }
+            public void Clear()
+            {
+                listOfI = new string[0];
+                iQuantity = new int[0];
+                ingredientM = new string[0];
+                steps = new string[0];
+
+                Console.WriteLine("recipe has been cleared");
+                Console.WriteLine("");
+                Console.WriteLine("would you like to make another recipe yes or no");
+                q3 = Console.ReadLine(); // taking input of user 
+
+                if (q3.ToLower() == "yes")
+                {
+                    Menu2 m = new Menu2();
+                }
+                else if (q3.ToLower() == "no")
+                {
+                    Console.WriteLine("thank you for using the application");
+                    System.Environment.Exit(1);
+                }
+
+            }
+
         }
-        public void Clear()
-        {
-             listOfI = new string[0];
-             iQuantity = new int[0];
-             ingredientM = new string[0];
-            steps = new string[0];
-
-            Console.WriteLine("recipe has been cleared");
-            Console.WriteLine("");
-            Console.WriteLine("would you like to make another recipe yes or no");
-            q3 = Console.ReadLine(); // taking input of user 
-
-            if (q3.ToLower() == "yes")
-            {
-                Menu2 m = new Menu2();
-            }
-            else if(q3.ToLower() == "no")
-            {
-                Console.WriteLine("thank you for using the application");
-                System.Environment.Exit(1);
-            }
-
-        }
-
-    }
 }
